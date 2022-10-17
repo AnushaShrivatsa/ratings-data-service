b1ea0a1847eac0e8a8108f8c236d94b9deae4461
AnushaShrivatsa <113424256+AnushaShrivatsa@users.noreply.github.com>
2022-10-17 19:24:07 +0530

    Initial commit

---
+++ src/main/java/com/io/java/ratingsdataservice/resource/RatingsDataResource.java
@@ -0,0 +1,36 @@
+package com.io.java.ratingsdataservice.resource;
+
+import com.io.java.ratingsdataservice.model.Rating;
+import com.io.java.ratingsdataservice.model.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsDataResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+
+        return new Rating(movieId,4);
+
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId){
+
+       // List<Rating> ratings = Arrays.asList(
+        //        new Rating("1234" , 4),
+        //        new Rating("5678" , 3)
+        //);
+
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+}
